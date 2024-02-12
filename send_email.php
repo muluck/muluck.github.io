d4e68a51a13492ba8e1aa52f781c69c7e5a78a49
muluck <123059313+muluck@users.noreply.github.com>
2024-02-12 14:09:53 +0100

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -5,15 +5,49 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $phone = $_POST['phone'];
     $message = $_POST['message'];
 
-    $to = 'linus.krenkel@hotmail.com'; // Setzen Sie hier die E-Mail-Adresse, an die die Nachricht gesendet werden soll.
+    $to = 'ziel@email.com'; // Setzen Sie hier die E-Mail-Adresse, an die die Nachricht gesendet werden soll.
     $subject = 'Nachricht von ' . $name;
     $body = "Name: $name\nEmail: $email\nTelefonnummer: $phone\n\nNachricht:\n$message";
 
-    // Senden der E-Mail
-    if (mail($to, $subject, $body)) {
-        echo '<script>alert("Ihre Nachricht wurde erfolgreich gesendet."); window.location = "contact.html";</script>';
-    } else {
-        echo '<script>alert("Es gab ein Problem beim Senden Ihrer Nachricht. Bitte versuchen Sie es später erneut."); window.location = "contact.html";</script>';
-    }
-}
-?>
+    // SendGrid-API-Schlüssel
+    $apiKey = "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";
+    
+    // Senden der E-Mail mit SendGrid
+    $url = 'https://api.sendgrid.com/v3/mail/send';
+    $data = array(
+        'personalizations' => array(
+            array(
+                'to' => array(
+                    array(
+                        'email' => $to
+                    )
+                ),
+                'subject' => $subject
+            )
+        ),
+        'from' => array(
+            'email' => $email
+        ),
+        'content' => array(
+            array(
+                'type' => 'text/plain',
+                'value' => $body
+            )
+        )
+    );
+    $headers = array(
+        'Content-Type: application/json',
+        'Authorization: Bearer ' . $apiKey
+    );
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    if ($response) {
+        echo '
